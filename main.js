5dd5a8b4295e34f1baba40c129d2296717031ab2
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-02-12 19:13:52 +0000

    feat: finish switch

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const htmlElement = document.documentElement;
+const isDarkTheme = htmlElement.classList.contains("dark");
+
+const svgIcon = document.getElementById("switch-icon");
+const baseSpritePath = "./assets/icons-sprite.svg";
+
+// set default href of icon
+svgIcon.firstElementChild.setAttribute(
+  "href",
+  `${baseSpritePath}#${isDarkTheme ? "moon" : "sun"}`
+);
 
 const toggleTheme = () => {
+  const useTag = svgIcon.firstElementChild;
+
   htmlElement.classList.toggle("dark");
+  // change icon when change theme
+  if (htmlElement.classList.contains("dark")) {
+    useTag.setAttribute("href", `${baseSpritePath}#moon`);
+  } else {
+    useTag.setAttribute("href", `${baseSpritePath}#sun`);
+  }
 };
+
+htmlElement.addEventListener("");
